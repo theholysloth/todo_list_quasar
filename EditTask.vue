cea1f47b0ca2989f16f6335b496015f16bda70b5
theholysloth <82107414+theholysloth@users.noreply.github.com>
2026-03-09 17:23:38 +0100

    Add files via upload
    passage à vuex

---
+++ EditTask.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page padding>
+    <task-form
+      :mode="mode"
+      :index="index"
+      v-bind="taskData"
+      @update-task="$emit('update-task', $event)"
+    />
+  </q-page>
+</template>
+
+<script>
+import TaskForm from 'src/components/TaskForm.vue'
+
+export default {
+  name: 'PageEditTask',
+  components: { TaskForm },
+
+  props: {
+    mode: String,
+    index: Number,
+    taskData: Object
+  }
+}
+</script>
